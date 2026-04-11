1784566a2c9a2774f16729d52e2ff69b3a9d2ce8
Oscar <osm1019@gmail.com>
2026-04-11 08:53:24 -0700

    add missing sepolicy
    Change-Id: I4f562850f86bbcf33656616afbdffbeee66972b0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,10 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
+# SEPolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/dodge/BoardConfigVendor.mk
 
