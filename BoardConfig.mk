eba3585d9c76ec5cb2a5b7b76d36760c0ac9ca16
Oscar <osm1019@gmail.com>
2026-06-17 21:54:48 -0700

    Add support for OPlus Camera

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,14 +29,11 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_UI_MARGIN_HEIGHT := 103
 
-# SEPolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-
 # Include the proprietary files BoardConfig.
 include vendor/oneplus/dodge/BoardConfigVendor.mk
 
+BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
+
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-
+BOARD_SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
